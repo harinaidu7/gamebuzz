53c28aebc4f67a701d01886204afbe0a3eadbf98
Krishnan <krishnannaidu704@gmail.com>
2022-09-08 18:33:48 +0530

    categories done

---
+++ src/components/Categories.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div style="background:#333; color:#fff">
+            <h2>Categories</h2>
+        </div>
+
+        <div class="container-fluid top-stories-main">
+            <div class="row">
+                <div class="col-3" v-for="types in categories" :key="types.id">
+                    <!-- <div class="col-3 gallery" v-for="list in topStories" v-bind:key="list.id"> -->
+
+                    <div>
+                        <!-- <a :href="`https://www.livescore.com/`+types.url">
+                            <img :src="types.mainMedia.thumbnail.url"/>
+                        </a>   -->
+                        <div class="decs">
+                            <a :href=" `/select/`+ types.id " >
+                                <!-- <div v-for="opt in categoriesList" :key="opt.id">
+                                    <div>{{Opt.id}}</div>
+                                </div> -->
+                               <img src='../assets/load.png' style="height:200px; width:180px"/>
+                               <div>{{types.title}}</div> 
+                            </a>
+                        </div>          
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+     import axios from "axios";
+export default {
+  name: "Categories",
+  data() {
+    return {
+      categories: [],
+      categoriesList:[]
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+
+  methods: {
+    async getData() {
+      let result = await axios.get(
+        "https://livescore6.p.rapidapi.com/news/v2/list",
+        {
+          headers: {
+            "X-RapidAPI-Key":
+            '21072ee344mshb713c007cd94c3fp189b06jsn6444bc28c7e9',
+            "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
+          },
+        }
+      );
+
+        // axios
+        //   .request(options)
+        //   .then(function(response) {
+        //     console.log(response.data);
+        //   })
+        //   .catch(function(error) {
+        //     console.error(error);
+        //   });
+      console.log(result.data);
+      this.categories = result.data.categories;
+    //   this.categoriesList= result.data.categories.id
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/topStories.vue
+++ src/components/topStories.vue
@@ -1,65 +1,38 @@
 <template>
     <div>
-        <div id="top-stories">
-            Top Stories
-        </div>
-        <!-- <div v-for="list in topStories" v-bind:key="list.id">
-            {{list.title}}
-        </div> -->
-        <div class="gallery" v-for="list in topStories" v-bind:key="list.id">
-            <a :href="`https://www.livescore.com/`+list.url">
-                <img :src="list.mainMedia.thumbnail.url"/>
-            </a>  
-            <div class="decs">{{list.title}}</div>          
-        </div>
+            <div class="top-stories-header" style="background:#333; color:#fff">
+                <h2>Top Stories</h2>
+            </div>
+            <div class="container-fluid top-stories-main">
+                <div class="row">
+                    <div class="col-3" v-for="list in topStories" :key="list.id">
+                        <!-- <div class="col-3 gallery" v-for="list in topStories" v-bind:key="list.id"> -->
 
-        
-        <div id="featured">
-            Featured
-        </div>
-        <div class="gallery" v-for="data in featuredArticles" :key="data.id">
-            <a :href="`https://www.livescore.com/`+data.url">
-                <img :src="data.mainMedia.thumbnail.url"/>
-            </a>  
-            <div class="decs">{{data.title}}</div>
-            
-        </div>
-    <!-- <div class="gallery">
-            <a href="#">
-                <img src=""/>
-            </a> 
-            <div class="decs">Add a description</div>            
-        </div>
-        <div class="gallery">
-            <a href="#">
-                <img src=""/>
-            </a>  
-            <div class="decs">Add a description</div>           
-        </div>
-        <div class="gallery">
-            <a href="#">
-                <img src=""/>
-            </a>  
-            <div class="decs">Add a description</div>           
-        </div>
-        <div class="gallery">
-            <a href="#">
-                <img src=""/>
-            </a> 
-            <div class="decs">Add a description</div>            
-        </div>
-        <div class="gallery">
-            <a href="#">
-                <img src=""/>
-            </a>  
-            <div class="decs">Add a description</div>           
-        </div>
-        <div class="gallery">
-            <a href="#">
-                <img src=""/>
-            </a>  
-            <div class="decs">Add a description</div>           
-        </div> -->
+                        <div>
+                            <!-- <a :href="`https://www.livescore.com/`+list.url"> -->
+                                <img :src="list.mainMedia.thumbnail.url"/>
+                            <!-- </a>   -->
+                            <div class="decs">
+                                {{list.title}}
+                            </div>          
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <!-- <div class="gallery" v-for="list in topStories" v-bind:key="list.id">
+                <div>
+                    <a :href="`https://www.livescore.com/`+list.url">
+                        <img :src="list.mainMedia.thumbnail.url"/>
+                    </a>  
+                    <div class="decs">
+                        {{list.title}}
+                    </div>          
+                </div>
+            </div> -->
+
     </div>
 </template>
 
@@ -69,48 +42,49 @@ export default {
   name: "TopStories",
   data() {
     return {
-        topStories:[],
-        featuredArticles:[]
+      topStories: [],
     };
   },
-  mounted(){
+  mounted() {
     this.getData();
   },
 
   methods: {
-   async getData() {
-      let result  = await axios.get("https://livescore6.p.rapidapi.com/news/v2/list",{
-        headers: {
-          "X-RapidAPI-Key":"2aa4c180a5msh4f3138b3ab5ddd2p19169ejsn9d3fe5173899",
-          "X-RapidAPI-Host": "livescore6.p.rapidapi.com"
+    async getData() {
+      let result = await axios.get(
+        "https://livescore6.p.rapidapi.com/news/v2/list",
+        {
+          headers: {
+            "X-RapidAPI-Key":
+            '21072ee344mshb713c007cd94c3fp189b06jsn6444bc28c7e9',
+            "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
+          },
         }
-    }
       );
 
-      
-      //   axios
-      //     .request(options)
-      //     .then(function(response) {
-          //       console.log(response.data);
-    //     })
-    //     .catch(function(error) {
-        //       console.error(error);
-        //     });
-        console.log(result.data)
-        this.topStories= result.data.topStories
-        this.featuredArticles= result.data.featuredArticles
-    }
-  }
-}
-
+      // axios
+      //   .request(options)
+      //   .then(function(response) {
+      //     console.log(response.data);
+      //   })
+      //   .catch(function(error) {
+      //     console.error(error);
+      //   });
+      console.log(result.data);
+      this.topStories = result.data.topStories;
+      this.featuredArticles = result.data.featuredArticles;
+      this.categories = result.data.categories;
+    },
+  },
+};
 </script>
 
 <style scoped>
-#top-stories {
-  background-color:#333;
+header{
+  background-color: #333;
   text-align: left;
   color: aliceblue;
-  padding: 7px
+  padding: 7px;
 }
 .gallery {
   margin: 5px;
@@ -133,11 +107,4 @@ div.gallery img {
   padding: 15px;
   text-align: center;
 }
-#featured{
-  background-color:#333;
-  text-align: left;
-  color: aliceblue;
-  padding: 7px
-}
-
 </style>
